c6a76851c1b7a67a445b49b2a111c553f8f685e2
akartal <akartal@uni-yaz.com>
2023-01-11 20:45:32 +0300

    initial commit

---
+++ src/main/java/com/uniyaz/sakila/core/country/CountryConverter.java
@@ -0,0 +1,30 @@
+package com.uniyaz.sakila.core.country;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CountryConverter {
+
+    public CountryDto convertToCountryDto(Country country) {
+
+        CountryDto countryDto = new CountryDto();
+        countryDto.setId(country.getId());
+        countryDto.setName(country.getCountry());
+        return countryDto;
+    }
+
+    public List<CountryDto> convertToCountryDtoList(List<Country> countryList) {
+
+        List<CountryDto> countryDtoList = new ArrayList<>();
+
+        for (Country country : countryList) {
+            CountryDto countryDto = convertToCountryDto(country);
+            countryDtoList.add(countryDto);
+        }
+
+        return countryDtoList;
+    }
+}
--- src/main/java/com/uniyaz/sakila/rest/CountryController.java
+++ src/main/java/com/uniyaz/sakila/rest/CountryController.java
@@ -1,12 +1,14 @@
 package com.uniyaz.sakila.rest;
 
 import com.uniyaz.sakila.core.country.Country;
+import com.uniyaz.sakila.core.country.CountryConverter;
+import com.uniyaz.sakila.core.country.CountryDto;
 import com.uniyaz.sakila.core.country.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +19,29 @@ public class CountryController {
     @Autowired
     CountryService countryService;
 
-    @GetMapping(path = "findAll", produces = MediaType.APPLICATION_PDF_VALUE)
+    @Autowired
+    CountryConverter countryConverter;
+
+    @GetMapping(path = "findAll")
     public List<Country> findAllCountry() {
         return countryService.findAll();
+    }
+
+    @GetMapping(path = "findAllByName", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity findAllCountry(String name) {
+        List<Country> countryList = countryService.findAllByName(name);
+        List<CountryDto> countryDtoList = countryConverter.convertToCountryDtoList(countryList);
+        if (!countryDtoList.isEmpty()) {
+            ResponseEntity responseEntity = new ResponseEntity(countryDtoList, HttpStatus.OK);
+            return responseEntity;
+        } else {
+            ResponseEntity responseEntity = new ResponseEntity(HttpStatus.NO_CONTENT);
+            return responseEntity;
+        }
+    }
+
+    @GetMapping(path = "findAllByName/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Country> findAllCountryPathVariable(@PathVariable String name) {
+        return countryService.findAllByName(name);
     }
 }
